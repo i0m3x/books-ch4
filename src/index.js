215352693cabebc3e683832a3d72e1ed9f23d108
lexiconPDX <63253189+lexiconPDX@users.noreply.github.com>
2020-08-29 11:02:31 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+//we are rendering 3 Book components inside the Library Component - 3 kids for the parent
+
 import React, { Component } from 'react'
 import {render} from 'react-dom'
 
-let bookList = [
+let bookList = [ //static book list - acts as a db
   {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260},
-  {"title": "Moby Dick", "author": "Ernest Hemingway", "pages": 4800},
+  {"title": "Moby Dick", "author": "Herman Melville", "pages": 4800},
   {"title": "Catcher in the Rye", "author": "J.D. Salinger", "pages": 220}
 ]
 
-const Book = ({title, author, pages, freeBookmark}) => {
+const Book = ({title, author, pages, freeBookmark}) => { //this is where we PASS / unpack the props, unpacking is optional, and it helps make it more readable
   return (
     <section>
       <h2>{title}</h2>
@@ -28,7 +30,7 @@ const NotHiring = () =>
   </div>
 
 
-class Library extends React.Component {
+class Library extends React.Component { //first step
   state = { 
     open: true,
     freeBookmark: true, 
@@ -43,17 +45,17 @@ class Library extends React.Component {
   }
   render() {
 
-  const {books} = this.props
+  const {books} = this.props //array of OBJECTS, but stored in an obj
   return (
     <div>
       {this.state.hiring ? <Hiring /> : <NotHiring />}
       <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
       <button onClick={this.toggleOpenClosed}>Change</button>
       {books.map(
-        (book, i) => 
-          <Book 
-          key={i}
-          title={book.title} 
+        (book, i) => //step, index - this 'i' is accessing the key below
+          <Book //calling a Book component
+          key={i} //this is the index
+          title={book.title} //for each of these objects, we are feeding in all of these props into the Book above
           author={book.author} 
           pages={book.pages}
           freeBookmark={this.state.freeBookmark}/>
